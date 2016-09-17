a9ad0758106a453ed1679294d7ee55cc6f6766ad
Ricardo Biehl Pasquali <rbpoficial@gmail.com>
2016-09-17 20:03:22 -0300

    doesn't set socket_priority
    I've also formated network.c file a little.
    socket_priority is not necessary I think

--- network.c
+++ network.c
@@ -29,51 +29,46 @@
 
 #define DEFAULT_FAMILY AF_INET
 
-static netlayer_t nl;
 netparam_t nlp;
+static netlayer_t nl;
 
-int socket_priority = 6; /* 6 = high priority */
+//int socket_priority = 6;  6 = high priority (TCP only)
 
 int
-netlayer_open(void)
+netlayer_open (void)
 {
 	struct sockaddr_in socket_addr;
+	int tmp_sfd; /* for TCP protocol */
 
-	int tmpsfd;
-
-	if((nl = socket(DEFAULT_FAMILY, nlp.socket_type, 0)) == -1)
+	if ((nl = socket(DEFAULT_FAMILY, nlp.socket_type, 0)) == -1)
 		return -1;
 
 	socket_addr.sin_family = DEFAULT_FAMILY;
 	socket_addr.sin_port = htons(nlp.port);
 
-	if(nlp.addr == NULL) /* if NULL we're in server mode */
-	{
+	if (nlp.addr == NULL) { /* if NULL we're in server mode */
 		socket_addr.sin_addr.s_addr = INADDR_ANY;
 
-		if(bind(nl, (struct sockaddr*) &socket_addr,
-		sizeof(struct sockaddr_in)) == -1)
+		if (bind(nl, (struct sockaddr*) &socket_addr,
+		    sizeof(struct sockaddr_in)) == -1)
 			return -1;
 
-		if(nlp.socket_type == SOCK_STREAM)
-		{
-			if(listen(nl, 1) == -1)
+		if (nlp.socket_type == SOCK_STREAM) {
+			if (listen(nl, 1) == -1)
 				return -1;
-
-			if((tmpsfd = accept(nl, NULL, NULL)) == -1)
+			if ((tmp_sfd = accept(nl, NULL, NULL)) == -1)
 				return -1;
 
 			close(nl);
-			nl = tmpsfd;
+			nl = tmp_sfd;
 		}
 	}
-	else /* else we're in client mode */
-	{
-		if((socket_addr.sin_addr.s_addr = inet_addr(nlp.addr)) == -1)
+	else { /* else we're in client mode */
+		if ((socket_addr.sin_addr.s_addr = inet_addr(nlp.addr)) == -1)
 			return -1;
 
-		if(connect(nl, (struct sockaddr*) &socket_addr,
-		sizeof(struct sockaddr_in)) == -1)
+		if (connect(nl, (struct sockaddr*) &socket_addr,
+		    sizeof(struct sockaddr_in)) == -1)
 			return -1;
 	}
 
@@ -81,26 +76,25 @@ netlayer_open(void)
 		/* set TCP_NODELAY option */
 		setsockopt(nl, IPPROTO_TCP, TCP_NODELAY, NULL, 0);
 
-	setsockopt(nl, SOL_SOCKET, SO_PRIORITY, &socket_priority, sizeof(int));
+	//setsockopt(nl, SOL_SOCKET, SO_PRIORITY, &socket_priority,sizeof(int));
 
 	return 0;
 }
 
 int
-netlayer_close(void)
+netlayer_close (void)
 {
 	return close(nl);
 }
 
 ssize_t
-netlayer_recv(void *buf, size_t len, int flags)
+netlayer_recv (void *buf, size_t len, int flags)
 {
 	ssize_t res, ret;
 
 	ret = len;
 
-	while(len)
-	{
+	while (len) {
 		if((res = read(nl, buf, len)) < 0)
 			return res; /* `res` serves to parse result */
 		len -= res;
@@ -111,15 +105,14 @@ netlayer_recv(void *buf, size_t len, int flags)
 }
 
 ssize_t
-netlayer_send(const void *buf, size_t len, int flags)
+netlayer_send (const void *buf, size_t len, int flags)
 {
 	ssize_t res, ret;
 
 	ret = len;
 
-	while(len)
-	{
-		if((res = write(nl, buf, len)) < 0)
+	while (len) {
+		if ((res = write(nl, buf, len)) < 0)
 			return res;
 		len -= res;
 		buf += res;
