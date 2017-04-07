6a5c7a8354bcf1c46915b8ac97c4837b972683b5
Taylor Holberton <taylorcholberton@gmail.com>
2017-04-06 21:25:58 -0700

    Added ipmic_device structure

--- audio.c
+++ audio.c
@@ -17,180 +17,278 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* stdio.h  for debug purposes */
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "audio.h"
 
-static audiolayer_t *al;
-audioparam_t alp;
+#include <stdlib.h>
 
-/*
- * See an example:
- *
- * If we have
- *  ______ ________ _______
- * |sample|channels|   rate|
- * | 16bit|     1ch|22050Hz|
- *
- * We'll have 44100 Bytes per second ( 2Bytes (16bit) * 1Channel * 22050Hz ).
- *
- * If we split a second of record in 20 parts
- *
- * ( 44100 Bytes/s  /  20 parts )  =  ( 2205 Bytes  /  1/20s )
- *
- * We'll have 2205 Bytes per 1/20 second.
- */
+#ifndef IPMIC_RELEASE
+/* for debugging */
+#include <stdio.h>
+#include <stdarg.h>
+#endif /* IPMIC_RELEASE */
 
-#ifndef _TINYALSA
+#ifdef IPMIC_WITH_TINYALSA
+#include "tinyalsa.h"
+#else /* IPMIC_WITH_TINYALSA */
+#include <alsa/asoundlib.h>
+#endif /* IPMIC_WITH_TINYALSA */
 
-static int
-set_hw_params(void)
+#ifndef IPMIC_RELEASE
+void
+ipmic_debug(const char *fmt, ...)
 {
-	int err;
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+}
+#else /* IPMIC_RELEASE */
+void
+ipmic_debug(const char *fmt, ...)
+{
+	(void) fmt;
+}
+#endif /* IPMIC_RELEASE */
 
+#ifdef IPMIC_WITH_TINYALSA
+
+struct ipmic_device
+{
+	/** A TinyALSA PCM */
+	struct pcm *pcm;
+};
+
+struct ipmic_device *
+ipmic_device_open(const struct ipmic_device_params *params)
+{
+	struct pcm *pcm;
+	struct pcm_config pcm_config;
+	int pcm_flags;
+	struct ipmic_device *dev;
+
+	pcm_config.channels = params->channels;
+	pcm_config.rate = params->rate;
+	pcm_config.format = PCM_FORMAT_S16_LE;
+	pcm_config.period_size = params->period_size;
+	pcm_config.period_count = 4;
+	pcm_config.start_threshold = 0;
+	pcm_config.stop_threshold = 0;
+	pcm_config.silence_threshold = 0;
+
+	if (params->type == IPMIC_DEVICE_INPUT)
+		pcm_flags = PCM_IN;
+	else
+		pcm_flags = PCM_OUT;
+
+	pcm = pcm_open(0, 0, pcm_flags, &pcm_config);
+	if (pcm == NULL)
+	{
+		ipmic_debug("%s: failed to malloc PCM\n", __func__);
+		return NULL;
+	}
+
+	if (!pcm_is_ready(pcm))
+	{
+		ipmic_debug("%s: failed to open PCM\n", __func__);
+		ipmic_debug("  %s\n", pcm_get_error(pcm));
+		pcm_close(pcm);
+		return NULL;
+	}
+
+	dev = malloc(sizeof(*dev));
+	if (dev == NULL)
+	{
+		ipmic_debug("%s: failed to malloc device\n", __func__);
+		return NULL;
+	}
+
+	dev->pcm = pcm;
+
+	return dev;
+}
+
+void
+ipmic_device_close(struct ipmic_device *device)
+{
+	if (device == NULL)
+	{
+		ipmic_debug("%s: received NULL device\n", __func__);
+		return;
+	}
+
+	if (device->pcm == NULL)
+	{
+		ipmic_debug("%s: received NULL device pcm\n", __func__);
+		return;
+	}
+
+	pcm_close(device->pcm);
+
+	free(device);
+}
+
+int
+ipmic_device_prepare(struct ipmic_device *device)
+{
+	return pcm_prepare(device->pcm);
+}
+
+int
+ipmic_device_read(struct ipmic_device *device,
+                  int16_t *frame_array,
+                  unsigned int frame_count)
+{
+	return pcm_read(device->pcm, frame_array, frame_count);
+}
+
+int
+ipmic_device_write(struct ipmic_device *device,
+                   const int16_t *frame_array,
+                   unsigned int frame_count)
+{
+	return pcm_write(device->pcm, frame_array, frame_count);
+}
+
+#else /* IPMIC_WITH_TINYALSA */
+
+struct ipmic_device {
+	/** An alsa-lib pcm */
+	snd_pcm_t *pcm;
+};
+
+int
+ipmic_device_set_params(struct ipmic_device *device,
+                        const struct ipmic_device_params *params)
+{
 	snd_pcm_hw_params_t *hw_params;
 
 	snd_pcm_hw_params_alloca(&hw_params);
-	snd_pcm_hw_params_any(al, hw_params);
 
-	/* Set access */
-	snd_pcm_hw_params_set_access(al, hw_params,
-	SND_PCM_ACCESS_RW_INTERLEAVED);
+	snd_pcm_hw_params_any(device->pcm, hw_params);
+	snd_pcm_hw_params_set_access(device->pcm, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED);
+	snd_pcm_hw_params_set_format(device->pcm, hw_params, SND_PCM_FORMAT_S16_LE);
+	snd_pcm_hw_params_set_channels(device->pcm, hw_params, params->channels);
+	snd_pcm_hw_params_set_rate(device->pcm, hw_params, params->rate, 0);
+	snd_pcm_hw_params_set_period_size(device->pcm, hw_params, params->period_size, 0);
+	snd_pcm_hw_params_set_periods(device->pcm, hw_params, 2, 0);
 
-	/* Set format */
-	snd_pcm_hw_params_set_format(al, hw_params, SND_PCM_FORMAT_S16_LE);
-
-	/* Set number of channels */
-	snd_pcm_hw_params_set_channels(al, hw_params, DEFAULT_CHANNELS);
-
-	/* Set rate */
-	snd_pcm_hw_params_set_rate(al, hw_params, DEFAULT_RATE, 0);
-
-	/* Set period size from parameters */
-	snd_pcm_hw_params_set_period_size(al, hw_params, alp.period_size, 0);
-
-	if((err = snd_pcm_hw_params(al, hw_params)) < 0)
-		return -1;
-
-	return 0;
+	return snd_pcm_hw_params(device->pcm, hw_params);
 }
 
-#endif /* ifndef _TINYALSA */
+struct ipmic_device *
+ipmic_device_open(const struct ipmic_device_params *params)
+{
+	int err;
+	snd_pcm_t *pcm;
+	snd_pcm_stream_t pcm_stream;
+	struct ipmic_device *dev;
+
+	if (params->type == IPMIC_DEVICE_INPUT)
+		pcm_stream = SND_PCM_STREAM_CAPTURE;
+	else
+		pcm_stream = SND_PCM_STREAM_PLAYBACK;
+
+	err = snd_pcm_open(&pcm, "default", pcm_stream, /* mode */ 0);
+	if (err != 0)
+	{
+		ipmic_debug("%s: failed to open pcm\n", __func__);
+		ipmic_debug("  %s\n", snd_strerror(err));
+		return NULL;
+	}
+
+	dev = malloc(sizeof(*dev));
+	if (dev == NULL)
+	{
+		err = errno;
+		ipmic_debug("%s: failed to malloc device\n", __func__);
+		ipmic_debug("  %s\n", strerror(err));
+		return NULL;
+	}
+
+	dev->pcm = pcm;
+
+	err = ipmic_device_set_params(dev, params);
+	if (err != 0)
+	{
+		ipmic_debug("%s: failed to set device parameters\n", __func__);
+		ipmic_debug("  %s\n", snd_strerror(err));
+		ipmic_device_close(dev);
+		return NULL;
+	}
+
+	return dev;
+}
+
+void
+ipmic_device_close(struct ipmic_device *device)
+{
+	if ((device != NULL) && (device->pcm != NULL))
+		snd_pcm_close(device->pcm);
+	free(device);
+}
+
+int
+ipmic_device_prepare(struct ipmic_device *device)
+{
+	return snd_pcm_prepare(device->pcm);
+}
+
+int
+ipmic_device_read(struct ipmic_device *device,
+                  int16_t *frame_array,
+                  unsigned int frame_count)
+{
+	return snd_pcm_readi(device->pcm, frame_array, frame_count);
+}
+
+int
+ipmic_device_write(struct ipmic_device *device,
+                   const int16_t *frame_array,
+                   unsigned int frame_count)
+{
+	return snd_pcm_writei(device->pcm, frame_array, frame_count);
+}
+
+#endif /* IPMIC_WITH_TINYALSA */
+
+struct ipmic_device_params alp;
+
+struct ipmic_device *ipmic_default_device;
 
 int
 audiolayer_open(void)
 {
-#ifdef _TINYALSA
-	struct pcm_config pcmconf;
-
-	/* Set format, channels, rate, period size and period count */
-	pcmconf.channels = DEFAULT_CHANNELS;
-	pcmconf.format = PCM_FORMAT_S16_LE;
-	pcmconf.rate = DEFAULT_RATE;
-	pcmconf.period_size = alp.period_size; /* in frames */
-	pcmconf.period_count = 2;
-	pcmconf.start_threshold = 0;
-	pcmconf.stop_threshold = 0;
-	pcmconf.silence_threshold = 0;
-
-	al = pcm_open(0, 0, (alp.type == AL_CAPTURE ? PCM_IN : PCM_OUT),
-	&pcmconf);
-
-	if(!pcm_is_ready(al))
-	{
-		fprintf(stderr, "tinyalsa Error: %s\n", pcm_get_error(al));
-		/* Do not close PCM!
-		 * pcm_is_ready() has returned '0' because `fd <= 0` */
-	}
-
-	return 0;
-_go_close_pcm:
-	pcm_close(al);
-	return -1;
-#else
-	if(snd_pcm_open(&al, "ipmic", (alp.type == AL_CAPTURE ?
-	SND_PCM_STREAM_CAPTURE : SND_PCM_STREAM_PLAYBACK), 0) < 0)
+	alp.channels = IPMIC_DEFAULT_CHANNELS;
+	alp.rate = IPMIC_DEFAULT_RATE;
+	ipmic_default_device = ipmic_device_open(&alp);
+	if (ipmic_default_device == NULL)
 		return -1;
-	if(set_hw_params() == -1)
-		goto _go_close_pcm;
-
+	ipmic_device_prepare(ipmic_default_device);
 	return 0;
-_go_close_pcm:
-	snd_pcm_close(al);
-	return -1;
-#endif
-}
-
-int
-audiolayer_close(void)
-{
-#ifdef _TINYALSA
-	pcm_close(al);
-#else
-	snd_pcm_close(al);
-#endif
-
-	return 0;
-}
-
-int
-audiolayer_read(void *buffer, unsigned int frames)
-{
-	int ret;
-
-	ret = frames;
-#ifdef _TINYALSA
-	if(pcm_read(al, buffer, frames_to_bytes(frames)))
-		return -1;
-#else
-	int res;
-
-	ret = frames;
-
-	while(frames)
-	{
-		if((res = snd_pcm_readi(al, buffer, frames)) < 0)
-			return res;
-		frames -= res;
-		buffer += frames_to_bytes(res);
-	}
-#endif
-
-	return ret;
-}
-
-int
-audiolayer_write(const void *buffer, unsigned int frames)
-{
-	int ret;
-
-	ret = frames;
-#ifdef _TINYALSA
-	if(pcm_write(al, buffer, frames_to_bytes(frames)))
-		return -1;
-#else
-	int res;
-
-	while(frames)
-	{
-		if((res = snd_pcm_writei(al, buffer, frames)) < 0)
-			return res;
-		frames -= res;
-		buffer += frames_to_bytes(res);
-	}
-#endif
-
-	return ret;
 }
 
 void
+audiolayer_close(void)
+{
+	ipmic_device_close(ipmic_default_device);
+	ipmic_default_device = NULL;
+}
+
+int
 audiolayer_prepare(void)
 {
-#ifdef _TINYALSA
-	pcm_prepare(al);
-#else
-	snd_pcm_prepare(al);
-#endif
+	return ipmic_device_prepare(ipmic_default_device);
 }
+
+int
+audiolayer_write(const void *frame_array, unsigned int frame_count)
+{
+	return ipmic_device_write(ipmic_default_device, frame_array, frame_count);
+}
+
+int
+audiolayer_read(void *frame_array, unsigned int frame_count)
+{
+	return ipmic_device_read(ipmic_default_device, frame_array, frame_count);
+}
+
--- audio.h
+++ audio.h
@@ -17,46 +17,148 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifdef _TINYALSA
-#include	"tinyalsa.h"
-#else
-#include	<alsa/asoundlib.h>
-#endif
+#ifndef IPMIC_AUDIO_H
+#define IPMIC_AUDIO_H
 
-#define AL_CAPTURE 0
-#define AL_PLAYBACK 1
+#include <stdint.h>
 
-#define DEFAULT_CHANNELS 1
-#define DEFAULT_RATE 22050
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
-#define bytes_to_frames(bytes) (bytes / 2 * DEFAULT_CHANNELS)
-#define frames_to_bytes(frames) (frames * 2 * DEFAULT_CHANNELS)
+#define IPMIC_DEFAULT_CHANNELS 2
+#define IPMIC_DEFAULT_RATE 48000
 
-#ifdef _TINYALSA
-typedef struct pcm audiolayer_t;
-#else
-typedef snd_pcm_t audiolayer_t;
-#endif
-typedef struct audiolayerparams_t audioparam_t;
+/** Describes whether an audio device is an input or output.
+ * Used when opening a device.
+ * */
 
-struct audiolayerparams_t
+enum ipmic_device_type
 {
-	int type; /* playback or capture */
-	int period_size; /* in frames */
-	int buffer_size; /* in bytes */
+	/** The device is an input device (e.g. microphone) */
+	IPMIC_DEVICE_INPUT,
+	/** The device is an output device (e.g. speaker) */
+	IPMIC_DEVICE_OUTPUT
 };
+
+/** Describes behaviour of a PCM.
+ * */
+
+struct ipmic_device_params
+{
+	/** Describes whether or not device is an input */
+	enum ipmic_device_type type;
+	/** The number of frames in a PCM period */
+	unsigned int period_size;
+	/** The number of frames in a PCM buffer */
+	unsigned int buffer_size;
+	/** The number of frames per second */
+	unsigned int rate;
+	/** The number of channels per frame */
+	unsigned int channels;
+};
+
+/** Represents an audio input or output.
+ * */
+
+struct ipmic_device;
+
+/** Opens up a audio device.
+ * @param params A structure containing
+ *  parameters affecting how the audio
+ *  device will run.
+ * @returns On success, a pointer to an audio device.
+ *  On failure, NULL.
+ * */
+
+struct ipmic_device *
+ipmic_device_open(const struct ipmic_device_params *params);
+
+/** Closes an audio device opened with @ipmic_device_open.
+ * @param device An initialized audio device.
+ *  This parameter may be NULL.
+ * */
+
+void
+ipmic_device_close(struct ipmic_device *device);
+
+/** Reads frames from an audio device.
+ * @param device An audio device opened
+ *  with @ref ipmic_device_open.
+ * @param frame_array A series of interleaved audio samples.
+ * @param frame_count The number of frames in @p frame_array.
+ *  This value should not be greater than INT_MAX.
+ * @retruns On success, the number of frames read.
+ *  On failure, a negative error code.
+ * */
+
+int
+ipmic_device_read(struct ipmic_device *device,
+                  int16_t *frame_array,
+                  unsigned int frame_count);
+
+/** Writes frames to an audio device.
+ * @param device An audio device opened
+ *  with @ref ipmic_device_open.
+ * @param frame_array A series of interleaved audio samples.
+ * @param frame_count The number of frames in @p frame_array.
+ *  This value should not be greater than INT_MAX.
+ * @retruns On success, the number of frames written.
+ *  On failure, a negative error code.
+ * */
+
+int
+ipmic_device_write(struct ipmic_device *device,
+                   const int16_t *frame_array,
+                   unsigned int frame_count);
+
+/** Prepares an audio device for input or output operations.
+ * @param device An audio device opened
+ *  with @ref ipmic_device_open.
+ * @returns Zero on success, a negative error code on failure.
+ * */
+
+int
+ipmic_device_prepare(struct ipmic_device *device);
+
+#define AL_PLAYBACK IPMIC_DEVICE_OUTPUT
+
+#define AL_CAPTURE IPMIC_DEVICE_INPUT
+
+#define DEFAULT_RATE IPMIC_DEFAULT_RATE
+
+#define DEFAULT_CHANNELS IPMIC_DEFAULT_CHANNELS
+
+#define bytes_to_frames(bytes) (bytes / 2 * IPMIC_DEFAULT_CHANNELS)
+
+#define frames_to_bytes(frames) (frames * 2 * IPMIC_DEFAULT_CHANNELS)
+
+typedef struct ipmic_device_params audioparam_t;
+
+extern struct ipmic_device_params alp;
+
+extern struct ipmic_device *ipmic_snd_input;
+
+extern struct ipmic_device *ipmic_snd_output;
 
 int
 audiolayer_open(void);
 
-int
+void
 audiolayer_close(void);
 
 int
-audiolayer_read(void*, unsigned int);
+audiolayer_read(void *, unsigned int);
 
 int
-audiolayer_write(const void*, unsigned int);
+audiolayer_write(const void *, unsigned int);
 
-void
+int
 audiolayer_prepare(void);
+
+#ifdef __cplusplus
+} /* extern "C" { */
+#endif /* __cplusplus */
+
+#endif /* IPMIC_AUDIO_H */
+
